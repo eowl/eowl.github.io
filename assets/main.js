918678602cd85c81244f64083e9d4bb7b4a1d551
Ran <owlran@outlook.com>
2025-02-05 23:29:45 +0800

    update

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   const images = document.querySelectorAll("img");
 
   images.forEach(img => {
-    // img.addEventListener("error", function() {
+    img.addEventListener("error", function() {
       if (img.src.includes("cdn.jsdelivr.net")) {
         img.src = img.src.replace("cdn.jsdelivr.net", "fastly.jsdelivr.net");
       }
-    // });
+    });
   });
 });
